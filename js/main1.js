d5add37818c324db9b31c6526adabdd2e8d73ead
isabella vazquez <62913519+isabellavaz@users.noreply.github.com>
2022-05-03 11:53:46 -0700

    Add files via upload

--- js/main1.js
+++ js/main1.js
@@ -1,110 +1,121 @@
 mapboxgl.accessToken =
-            'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/Moonlight',
-            zoom: 4, // starting zoom
-            center: [-100, 40], // starting center
-            projection: 'albers' // make map albers projection 
-        });
+    'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/dark-v10',
+    zoom: 3.5, // starting zoom
+    center: [-100, 40], // starting center
+    projection: 'albers' // make map albers projection
 
-async function geojsonFetch() { 
-// other operations
-};
+});
 
-
-let response = await fetch('geog458-covidmap/assets/covidcount.geojson');
-let covidcount = await response.json();
-
-    const case_numbers = [5000, 20000, 40000],
-        colors= ['rgb(255, 237, 160)', 'rgb(254, 178, 76)', 'rgb(240, 59, 32)'],
-        radii = [5, 15, 20];
+async function geojsonFetch() {
+    // other operations
+    let response = await fetch('../assets/covidrates.json');
+    let covidrates = await response.json();
 
 
+    //load data to the map as new layers.
     //map.on('load', function loadingData() {
-    map.on('load', function loadingdata() {//simplifying the function statement: arrow with brackets to define a function
+    map.on('load', function loadingdata() { //simplifying the function statement: arrow with brackets to define a function
 
         // when loading a geojson, there are two steps
         // add a source of the data and then add the layer out of the source
-        map.addSource('covidcount', {
+        map.addSource('covidrates', {
             type: 'geojson',
-            data: covidcount
+            data: covidrates
         });
 
         map.addLayer({
-            'id': 'covid-count-layer',
-            'type': 'circle',
-            'source': 'covidcount',
+            'id': 'covidrates-layer',
+            'type': 'fill',
+            'source': 'covidrates',
             'paint': {
-                    // increase the radii of the circle as the zoom level and dbh value increases
-                    'circle-radius': {
-                        'property': 'cases',
-                        'stops': [
-                            [{
-                                zoom: 5,
-                                value: case_numbers[0]
-                            }, radii[0]],
-                            [{
-                                zoom: 5,
-                                value: case_numbers[1]
-                            }, radii[1]],
-                            [{
-                                zoom: 5,
-                                value: case_numbers[2]
-                            }, radii[2]]
-                        ]
-                    },
-                    'circle-color': {
-                        'property': 'cases',
-                        'stops': [
-                            [case_numbers[0], colors[0]],
-                            [case_numbers[1], colors[1]],
-                            [case_numbers[2], colors[2]]
-                        ]
-                    },
-                    'circle-stroke-color': 'white',
-                    'circle-stroke-width': 1,
-                    'circle-opacity': 0.6
-                }
-            },
-            'waterway-label'
-        );
+                // increase the radii of the circle as the zoom level and dbh value increases
+                'fill-color': [
+                    'step',
+                    ['get', 'rates'],
 
-        // click on tree to view magnitude in a popup
-        map.on('click', 'covid-count-layer', (event) => {
-            new mapboxgl.Popup()
-                .setLngLat(event.features[0].geometry.coordinates)
-                .setHTML(`<strong>Cases:</strong> ${event.features[0].properties.cases}`)
-                .addTo(map);
-        });            
+                    '#FFEDA0', // stop_output_0
+                    10, // stop_input_0
+                    '#FED976', // stop_output_1
+                    20, // stop_input_1
+                    '#FEB24C', // stop_output_2
+                    50, // stop_input_2
+                    '#FD8D3C', // stop_output_3
+                    100, // stop_input_3
+                    '#FC4E2A', // stop_input_4
+                    200, // stop_input_4
+                    '#E31A1C', // stop_output_5
+                    500, // stop_input_5
+                    "#BD0026", // stop_output_6
+                    1000, // stop_input_6
+                    "#800026", // stop_output_7
+
+                ],
+                'fill-outline-color': '#BBBBBB',
+                'fill-opacity': 0.7,
+            }
+
+
+
+
+        });
+
+        const layers = [
+            '0-9',
+            '10-19',
+            '20-49',
+            '50-99',
+            '100-199',
+            '200-499',
+            '500-999',
+            '1000+'
+        ];
+        const colors = [
+            '#FFEDA070',
+            '#FED97670',
+            '#FEB24C70',
+            '#FD8D3C70',
+            '#FC4E2A70',
+            '#E31A1C70',
+            '#BD002670',
+            '#80002670'
+        ];
+
+
+        // create legend
+        const legend = document.getElementById('legend');
+        // add the data source
+        const source =
+            '<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">NY Times</a> </p>';
+        legend.innerHTML = "<b>COVID-19 Rates in the U.S.<br>(cases in 1,000 people)" + source;
+
+        layers.forEach((layer, i) => {
+            const color = colors[i];
+            const item = document.createElement('div');
+            const key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
+
+            const value = document.createElement('span');
+            value.innerHTML = `${layer}`;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+        });
+
+        map.on('mousemove', (event) => {
+            const crates = map.queryRenderedFeatures(event.point, {
+                layers: ['covidrates-layer']
+            });
+            document.getElementById('text-description').innerHTML = crates.length 
+            ? `<h3>${crates[0].properties.county} County</h3><p><strong><em>${crates[0].properties.rates}</strong> cases per 1000 people</em></p>` :
+                `<p>Hover over a county!</p>`;
+        });
 
     });
 
+};
 
-    // create legend
-    const legend = document.getElementById('legend');
-
-    //set up legend case numbers and labels
-    var labels = ['<strong>COVID-19 Case Number </strong>'], vbreak;
-    //iterate through grades and create a scaled circle and label for each
-    for (var i = 0; i < case_numbers.length; i++) {
-        vbreak = case_numbers[i];
-        // you need to manually adjust the radius of each dot on the legend 
-        // in order to make sure the legend can be properly referred to the dot on the map.
-        dot_radii = 2 * radii[i];
-        labels.push(
-            '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
-            'px; height: ' +
-            dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
-            '</span></p>');
-
-    }
-
-    // add the data source
-    const source =
-        '<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">NY Times</a> </p>';
-   
-    // combine all the html codes.
-    legend.innerHTML = labels.join('') + source;
-
-    geojsonFetch();
+geojsonFetch();
--- js/main2.js
+++ js/main2.js
@@ -1,125 +1,102 @@
 mapboxgl.accessToken =
-            'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/Moonlight',
-            zoom: 4, // starting zoom
-            center: [-100, 40], // starting center
-            projection: 'albers' // make map albers projection
-  
-        });
-
-async function geojsonFetch() { 
-// other operations
-};
+    'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+let map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/dark-v10',
+    zoom: 3.5, // starting zoom
+    center: [-100, 40], // starting center
+    projection: 'albers' // make map albers projection 
+});
 
 
+const case_numbers = [5000, 20000, 40000],
+    colors = ['rgb(255, 237, 160)', 'rgb(254, 178, 76)', 'rgb(240, 59, 32)'],
+    radii = [5, 15, 20];
 
-let response = await fetch('assets/covidrates.geojson');
-let covidrates = await response.json();
 
-//load data to the map as new layers.
 //map.on('load', function loadingData() {
-    map.on('load', function loadingdata() {//simplifying the function statement: arrow with brackets to define a function
+map.on('load', function loadingdata() { //simplifying the function statement: arrow with brackets to define a function
 
-        // when loading a geojson, there are two steps
-        // add a source of the data and then add the layer out of the source
-        map.addSource('covid-rate', {
-            type: 'geojson',
-            data: covidrates
-        });
-        
-        map.addLayer({
-                'id': 'covidrates-layer',
-                'type': 'fill',
-                'source': 'covid-rate',
-                'paint': {
-                    // increase the radii of the circle as the zoom level and dbh value increases
-                    'fill-color': [
-                        'step',
-                        ['get', 'rates'],
-        
-                        '#feebe2', // stop_output_0
-                        21, // stop_input_0
-                        '#fcc5c0', // stop_output_1
-                        41, // stop_input_1
-                        '#fa9fb5', // stop_output_2
-                        61, // stop_input_2
-                        '#f768a1', // stop_output_3
-                        91, // stop_input_3
-                        '#dd3497', // stop_input_4
-                        131, // stop_input_4
-                        '#ae017e', // stop_output_5
-                        171, // stop_input_5
-                        "#7a0177", // stop_output_6
-                        201, // stop_input_6
-                        "#b10026", // stop_output_7
-        
-                    ],
-                    'fill-outline-color': '#BBBBBB',
-                    'fill-opacity': 0.7,
-                }
-        
-        
-            },
-        
-        );
-        const layers = [
-            '0-20',
-            '21-40',
-            '41-60',
-            '61-90',
-            '91-130',
-            '131-170',
-            '171-200',
-            '201 and more'
-        ];
-        const colors = [
-            '#feebe270',
-            '#fcc5c070',
-            '#fa9fb570',
-            '#f768a170',
-            '#dd349770',
-            '#ae017e70',
-            '#7a017770',
-            '#b1002670'
-        ];
-        // create legend
-        const legend = document.getElementById('legend');
-        legend.innerHTML = "<b>COVID-19 Rates" + source;
-        
-        layers.forEach((layer, i) => {
-            const color = colors[i];
-            const item = document.createElement('div');
-            const key = document.createElement('span');
-            key.className = 'legend-key';
-            key.style.backgroundColor = color;
-        
-            const value = document.createElement('span');
-            value.innerHTML = `${layer}`;
-            item.appendChild(key);
-            item.appendChild(value);
-            legend.appendChild(item);
-        });
-        
-        map.on('mousemove', ({point}) => {
-            const state = map.queryRenderedFeatures(point, {
-                layers: ['covidrates-layer']
-            });
-            document.getElementById('text-description').innerHTML = state.length ?
-                `<h3>${state[0].properties.name}</h3><p><strong><em>${state[0].properties.density}</strong> cases per 1000 people</em></p>` :
-                `<p>Hover over a county!</p>`;
-        });
-        
-        
-        
-        });
-        
-        // add the data source
-        const source =
-            '<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">NY Times</a> </p>';
-        // combine all the html codes.
-        legend.innerHTML = labels.join('') + source;
-        
-        geojsonFetch();
-    
+    // when loading a geojson, there are two steps
+    // add a source of the data and then add the layer out of the source
+    map.addSource('covidcount', {
+        type: 'geojson',
+        data: '../assets/covidcount.json',
+    });
+
+    map.addLayer({
+            'id': 'covidcount-layer',
+            'type': 'circle',
+            'source': 'covidcount',
+            'paint': {
+                // increase the radii of the circle as the zoom level and dbh value increases
+                'circle-radius': {
+                    'property': 'cases',
+                    'stops': [
+                        [{
+                            zoom: 5,
+                            value: case_numbers[0]
+                        }, radii[0]],
+                        [{
+                            zoom: 5,
+                            value: case_numbers[1]
+                        }, radii[1]],
+                        [{
+                            zoom: 5,
+                            value: case_numbers[2]
+                        }, radii[2]]
+                    ]
+                },
+                'circle-color': {
+                    'property': 'cases',
+                    'stops': [
+                        [case_numbers[0], colors[0]],
+                        [case_numbers[1], colors[1]],
+                        [case_numbers[2], colors[2]]
+                    ]
+                },
+                'circle-stroke-color': 'white',
+                'circle-stroke-width': 1,
+                'circle-opacity': 0.6
+            }
+        },
+        'waterway-label'
+    );
+
+    // click on tree to view magnitude in a popup
+    map.on('click', 'covidcount-layer', (event) => {
+        new mapboxgl.Popup()
+            .setLngLat(event.features[0].geometry.coordinates)
+            .setHTML(`<strong>County:</strong> ${event.features[0].properties.county}, ${event.features[0].properties.state}
+                    <br><strong>Cases:</strong> ${event.features[0].properties.cases}`)
+            .addTo(map);
+    });
+
+});
+
+// create legend
+const legend = document.getElementById('legend');
+
+//set up legend case numbers and labels
+var labels = ['<strong>COVID-19 Case Number </strong>'],
+    vbreak;
+//iterate through grades and create a scaled circle and label for each
+for (var i = 0; i < case_numbers.length; i++) {
+    vbreak = case_numbers[i];
+    // you need to manually adjust the radius of each dot on the legend 
+    // in order to make sure the legend can be properly referred to the dot on the map.
+    dot_radii = 2 * radii[i];
+    labels.push(
+        '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
+        'px; height: ' +
+        dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
+        '</span></p>');
+
+}
+
+// add the data source
+const source =
+    '<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">NY Times</a> </p>';
+
+// combine all the html codes.
+legend.innerHTML = labels.join('') + source;
